f35e7224cf50cbc584bb6355ffeef5b767659913
Dev <joisardev@gmail.com>
2022-01-03 18:50:38 +0530

    Shows a Decrementing FOR loop

---
+++ Loop_Decement.c
@@ -0,0 +1,19 @@
+//Program to demostrate decrement loop
+
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter number = ");
+    scanf("%d",&n);
+    for ( i=n;i>=1;i--)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
+
+
+
+
