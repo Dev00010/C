d695393a3e8c98be92cb929779c073f0ac75310c
Dev <joisardev@gmail.com>
2022-01-03 16:13:39 +0530

    My First Program

---
+++ 1st-user-input_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("\nEnter no1=");
+    scanf("%d",&a);
+    printf("\nEnter no2=");
+    scanf("%d",&b);
+    
+    printf("Add=%d",a+b);
+
+    return 0;
+}
