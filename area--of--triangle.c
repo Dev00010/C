9509535d836c6b859cd865770bdfe4066639c1b4
Dev <joisardev@gmail.com>
2022-01-03 16:22:42 +0530

    Prints Area of Triangle with pre-defined dimension

---
+++ area--of--triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int length,breadth,height,area;
+    length=10;
+    breadth=20;
+    height=30;
+    area=length*breadth*height;
+    printf("area=%d",area);
+
+    return 0;
+}
