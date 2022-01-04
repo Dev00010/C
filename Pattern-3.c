cf40f98e708eea4a9e1da884f503e9a801569657
Dev <joisardev@gmail.com>
2022-01-04 12:20:43 +0530

    Pattern Program 3

---
+++ Pattern-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	int i,n,no,j;
+	printf("\nEnter limit=> ");
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+		printf("* ");
+	    }
+		printf("\n"); 
+	}
+
+
+	
+}
