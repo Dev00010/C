1b4e4a9bbd805c61e3d5c994fbc45233b530a717
Dev <joisardev@gmail.com>
2022-01-04 12:30:53 +0530

    Pattern Program 6

---
+++ Pattern-6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	int i,n,no,j;
+	printf("\nEnter limit=> ");
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		for(j=1;j<=no;j++)
+		{
+		printf("%d",i);
+	    }
+		printf("\n"); 
+	}
+
+
+	
+}
