43f625e6753b36282c186d5921418326d41b45ff
Dev <joisardev@gmail.com>
2022-01-04 11:14:59 +0530

    Reads user input for Matrics and prints it

---
+++ Matrices_Print.c
@@ -0,0 +1,24 @@
+//Prgram to take input in matrices and print it
+
+#include<stdio.h>
+main()
+{
+	int a[3][3];
+	int i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+	     printf("\nEnter number a[%d][%d]=> ",i,j);
+	     scanf("%d",&a[i][j]);
+        }
+	}
+    for(i=0;i<3;i++)
+    {
+    	for(j=0;j<3;j++)
+    	{
+    		printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
